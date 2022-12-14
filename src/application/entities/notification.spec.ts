bca6b11547194519fcb064a5003491ec5ca9e900
Valdir Silva <valdipiresba@gmail.com>
2022-12-13 22:01:30 -0300

    Create tests notification

---
+++ src/application/entities/notification.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from "./content";
+import { Notification } from "./Notification";
+
+describe('Notification content', () => {
+    it('should be able to create a notification', () => {
+        const notification = new Notification({
+            content: new Content('Nova solicitação de amizade'),
+            category: 'social',
+            recipientId: 'example.recipient-id'
+        })
+
+        expect(notification).toBeTruthy();
+    });
+
+});
